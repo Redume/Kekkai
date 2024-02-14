3df3b9efb6e247b6e8cfd248f2d13cb2a8153a17
Redume <danilivasenko09@gmail.com>
2024-02-14 15:11:12 +0300

    добавлен подхват данных из конфига для удобства

--- postgresql.js
+++ postgresql.js
@@ -1,8 +1,14 @@
 const pg = require("pg");
+const yaml = require("yaml")
+const fs = require("fs");
+const config = yaml.parse(fs.readFileSync("./config.yaml", "utf-8"));
+
 const pool = new pg.Pool({
-    user: "",
-    password: "",
-    host: "",
-    port: 5432,
-    database: ""
-})
+    user:  config['db_user'],
+    password: config['db_password'],
+    host: config['db_host'],
+    port: config['db_port'],
+    database: config['db_name']
+});
+
+module.exports = pool;
