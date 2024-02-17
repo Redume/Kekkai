d5c76d03c8447e946977a4f02b95e2bc5a7c5422
Redume <danilivasenko09@gmail.com>
2024-02-17 14:17:13 +0300

    добавлена поддержка прокси и изменен формат времени

--- utils/saveCourse.js
+++ utils/saveCourse.js
@@ -2,29 +2,42 @@ const pool = require("../postgresql.js");
 const yaml = require("yaml")
 const fs = require("fs");
 const axios = require("axios");
+const {AxiosProxyConfig} = require("axios");
 const config = yaml.parse(fs.readFileSync("./config.yaml", "utf-8"));
 
-async function saveCourse() {
+async function saveRate() {
     config['currency'].forEach(
         (value) => config['currency'].forEach(async (pair) => {
             if(value !== pair) {
                 const res = await axios.get(
                     `https://duckduckgo.com/js/spice/currency/1/${value}/${pair}`,
                     {
-                        timeout: 3000
+                        timeout: 3000,
                     }
-                )
-                const data = JSON.parse(res.data.replace('ddg_spice_currency(', '').replace(');', ''))
+                );
+
+                if (!config['proxy']['host']) {
+                    res.config.proxy = {
+                        protocol: config['proxy']['protocol'],
+                        host: config['proxy']['host'],
+                        port: config['proxy']['port'],
+                        auth: {
+                            username: config['proxy']['auth']['username'],
+                            password: config['proxy']['auth']['password'],
+                        }
+                    }
+                }
+
+                const data = JSON.parse(res.data.replace('ddg_spice_currency(', '').replace(');', ''));
                 delete data['terms'];
                 delete data['privacy'];
-                console.log(data)
 
-                const point = data['to'][0]['mid'].toString().indexOf('.') + 4
+                const point = data['to'][0]['mid'].toString().indexOf('.') + 4;
 
                 pool.query('SELECT * FROM currency WHERE from_currency = $1 AND date = $2',
                         [
                             value,
-                            data['timestamp']
+                            new Date(data['timestamp']).toLocaleDateString()
                         ]
                     ).then(async (db) => {
                         if (!db['rows'][0]) {
@@ -34,14 +47,14 @@ async function saveCourse() {
                                     value,
                                     pair,
                                     data['to'][0]['mid'].toString().slice(0, point),
-                                    data['timestamp']
+                                    new Date(data['timestamp']).toLocaleDateString()
                                 ]
-                            )
+                            );
                         }
-                })
+                });
             }
         })
     );
 }
 
-module.exports = saveCourse;
+module.exports = saveRate;
