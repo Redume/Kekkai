dd3311b5ae15cbe85c77fef92889d32a99086800
Redume <danilivasenko09@gmail.com>
2024-03-24 15:46:03 +0300

    добавлена таблица с криптовалютой

--- data/schema.sql
+++ data/schema.sql
@@ -1,6 +1,13 @@
-CREATE TABLE currency(
+CREATE TABLE fiat(
     from_currency TEXT NOT NULL,
     conv_currency TEXT NOT NULL,
     rate FLOAT NOT NULL,
-    date TEXT NOT NULL
+    date DATE NOT NULL
+);
+
+CREATE TABLE crypto(
+    from_currency TEXT NOT NULL,
+    conv_currency TEXT NOT NULL,
+    rate FLOAT NOT NULL,
+    date DATE NOT NULL
 );
