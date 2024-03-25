6d3a98f3801f8053839b5b17495f7ced5891f56b
Redume <danilivasenko09@gmail.com>
2024-03-25 13:22:42 +0300

    Повышение читабельности кода

--- dao/Currency.js
+++ dao/Currency.js
@@ -1,6 +1,6 @@
 const pool = require('../postgresql.js');
 
-const Currency = class Currency {
+class Currency {
     constructor(table, fromCurrency, convCurrency, periodStart, periodEnd) {
         this.table = table;
         this.fromCurrency = fromCurrency;
@@ -13,11 +13,10 @@ const Currency = class Currency {
         let data = await pool.query(`SELECT from_currency, conv_currency, rate, date FROM ${this.table} 
                                         WHERE from_currency = $1
                                             AND conv_currency = $2 
-                                            AND date = $3`,
-            [
-                this.fromCurrency,
-                this.convCurrency,
-                this.periodStart
+                                            AND date = $3`, [
+                                                this.fromCurrency,
+                                                this.convCurrency,
+                                                this.periodStart
             ]);
 
         if (!data?.rows[0]) return {};
@@ -26,54 +25,51 @@ const Currency = class Currency {
             const data = await pool.query(`SELECT from_currency, conv_currency, rate, date FROM ${this.table} 
                                         WHERE (date BETWEEN $3 AND $4) 
                                             AND from_currency = $1 
-                                            AND conv_currency = $2`,
-                [
-                    this.fromCurrency,
-                    this.convCurrency,
-                    this.periodStart,
-                    this.periodEnd,
+                                            AND conv_currency = $2`, [
+                                                this.fromCurrency,
+                                                this.convCurrency,
+                                                this.periodStart,
+                                                this.periodEnd,
                 ]);
 
-            if (!data?.rows[0]) return {}
+            if (!data?.rows[0]) return {};
 
             for (let i = 0; i < data.rows.length; i++) {
-                const date = data.rows[0].date
+                const date = data.rows[0].date;
                 data.rows[0].date = new Date(date.setDate(date.getDate() + 1)).toISOString();
             }
 
             return data.rows;
         }
 
-        const date = data.rows[0].date
+        const date = data.rows[0].date;
         data.rows[0].date = new Date(date.setDate(date.getDate() + 1)).toISOString();
 
-        return data.rows
+        return data.rows;
     };
 
     async setData(rate) {
         const data = await pool.query(`SELECT from_currency, conv_currency, rate, date FROM ${this.table} 
                                                 WHERE from_currency = $1 
                                                   AND conv_currency = $2 
-                                                  AND date = $3`,
-            [
-                this.fromCurrency,
-                this.convCurrency,
-                this.periodStart
+                                                  AND date = $3`, [
+                                                        this.fromCurrency,
+                                                        this.convCurrency,
+                                                        this.periodStart
             ]);
 
-        if (data?.rows[0]) return false
+        if (data?.rows[0]) return false;
 
         await pool.query(`INSERT INTO ${this.table} (from_currency, conv_currency, rate, date) 
                             VALUES ($1, $2, $3, $4) 
-                            RETURNING from_currency, conv_currency, rate, date`,
-            [
-                this.fromCurrency,
-                this.convCurrency,
-                rate,
-                this.periodStart
-            ])
+                            RETURNING from_currency, conv_currency, rate, date`, [
+                                this.fromCurrency,
+                                this.convCurrency,
+                                rate,
+                                this.periodStart
+            ]);
 
-        return true
+        return true;
     };
 }
 
