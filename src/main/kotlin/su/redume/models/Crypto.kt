85d7d085884b30096c52b1d1b40a5d478155055f
Redume <danilivasenko09@gmail.com>
2024-04-18 21:21:47 +0300

    добавлены схемы sql таблиц

--- src/main/kotlin/su/redume/models/Crypto.kt
+++ src/main/kotlin/su/redume/models/Crypto.kt
@@ -6,6 +6,6 @@ import org.jetbrains.exposed.sql.stringParam
 object Crypto : Table() {
     val fromCurrecny = stringParam("fromCurrecny")
     val convCurrency = stringParam("convCurrency")
-    val rate = integer("rate")
+    val rate = float("rate")
     val date = long("date")
 }
