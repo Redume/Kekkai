3c579716c632fe7063ef6cd7e7f14c4ef33beb53
Redume <danilivasenko09@gmail.com>
2024-04-19 18:09:10 +0300

    Пофиксил создание столбцов

--- src/main/kotlin/su/redume/models/Crypto.kt
+++ src/main/kotlin/su/redume/models/Crypto.kt
@@ -1,11 +1,10 @@
 package su.redume.models
 
 import org.jetbrains.exposed.sql.Table
-import org.jetbrains.exposed.sql.stringParam
 
 object Crypto : Table() {
-    val fromCurrecny = stringParam("fromCurrecny")
-    val convCurrency = stringParam("convCurrency")
+    val fromCurrecny = varchar("fromCurrecny", 3)
+    val convCurrency = varchar("convCurrency", 3)
     val rate = float("rate")
     val date = long("date")
 }
