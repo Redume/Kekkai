0ed7b718f378cafe5b240d63a7ddc55145e3b47e
Redume <danilivasenko09@gmail.com>
2024-06-24 21:54:06 +0300

    manual revert commit

--- src/main/kotlin/su/redume/core/network/api/FiatCurrencyServer.kt
+++
@@ -1,7 +0,0 @@
-package su.redume.core.network.api
-
-import su.redume.core.network.model.CurrencyDto
-
-interface FiatCurrencyServer {
-    suspend fun currency(value: String, pair: String): List<CurrencyDto>
-}
--- src/main/kotlin/su/redume/dto/Fiat.kt
+++
@@ -1,6 +0,0 @@
-package su.redume.dto
-
-import kotlinx.serialization.Serializable
-
-@Serializable
-data class Fiat(val fromCurrency: String, val convCurrency: String, val rate: Float, val date: String)
