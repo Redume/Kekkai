c5fa45c0fa0e8a1c73feb31901fc39c71e04867b
Redume <danilivasenko09@gmail.com>
2024-07-30 18:06:21 +0300

    Главный сервис, где выдается данные о курсе валюты

---
+++ server/main.js
@@ -0,0 +1,23 @@
+const fastify = require('fastify')({logger: true})
+const rate = require('../database/main.js');
+
+fastify.get('/api/getRate/', async function (req, res){
+    const query = req.query;
+    if (query['fromCurrency'] || query['convCurrency']) return;
+
+    if (query['date']) return rate.getDay(query['fromCurrency'], query['convCurrency'], query['date']);
+    else if (query['startDate'] && query['endDate']) return rate.getPeriod(
+        query['fromCurrency'],
+        query['convCurrency'],
+        query['startDate'],
+        query['endDate']
+    );
+    else return;
+});
+
+fastify.listen({ port: 3000 }, function (err) {
+    if (err) {
+        fastify.log.error(err)
+        process.exit(1)
+    }
+});
