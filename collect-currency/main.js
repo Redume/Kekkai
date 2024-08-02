6d241ffd11fe061758cfc6d6741447022eb3203a
Redume <danilivasenko09@gmail.com>
2024-08-02 15:40:44 +0300

    Сделал управлением сбором валют

---
+++ collect-currency/main.js
@@ -0,0 +1,19 @@
+const schedule = require('node-schedule');
+const config = require('../config/main.js')();
+const cron = require('cron-validator');
+
+const save_fiat = require('./save_fiat');
+const save_crypto = require('./save_crypto');
+
+async function main() {
+    const config_schedule = config['currency']['collecting']['schedule'];
+    if (!config_schedule) throw new Error('The crontab schedule is not set');
+    if (!cron.isValidCron(config_schedule, {alias: true})) throw new Error('The crontab is invalid');
+
+    schedule.scheduleJob(config_schedule, async function () {
+        await save_fiat();
+        await save_crypto();
+    });
+}
+
+main();
