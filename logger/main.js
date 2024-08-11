1b785f5acc299080363f5ebb32a0ff2e8c27f2ce
Redume <danilivasenko09@gmail.com>
2024-08-11 17:21:20 +0300

    Если в конфиге включен, то будет выводится дебаг инфа в консоль

--- logger/main.js
+++ logger/main.js
@@ -1,6 +1,9 @@
 const pino = require('pino');
 const pretty = require('pino-pretty');
+const config = require('../config/main.js')();
 
-const logger = pino(pretty());
+const logger = pino({
+    level: config['server']['debug'] ? 'debug' : 'info',
+    }, pretty());
 
 module.exports = logger;
