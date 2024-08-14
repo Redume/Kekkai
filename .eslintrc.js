bfccc5aac28d28cedbfccc70a048a946c94f1c47
Redume <danilivasenko09@gmail.com>
2024-08-14 21:19:50 +0300

    Добавил eslint, prettier

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+    env: {
+        browser: true,
+        node: true,
+        es6: true
+    },
+    extends: ['eslint:recommended', 'plugin:prettier/recommended'],
+    parserOptions: {
+        'sourceType': 'commonjs',
+        "ecmaVersion": 'latest'
+    },
+    rules: {
+        indent: 'off',
+        semi: ['error', 'always'],
+        'arrow-body-style': ["error", "as-needed"],
+        "prettier/prettier": [
+          "error", {
+              "singleQuote": true,
+              "parser": "flow"
+          }
+          ],
+    },
+    overrides: [
+        {
+            'files': ['*/*.js'],
+        },
+    ],
+    'ignorePatterns': ['.eslintrc.js'],
+};
