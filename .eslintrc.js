bdc4450b728efa2ababdb7d3caea0c198f8e64e8
Redume <danilivasenko09@gmail.com>
2024-08-17 16:35:16 +0300

    Добавил размер табуляции, убрал одинарные кавычки

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,24 +6,25 @@ module.exports = {
     },
     extends: ['eslint:recommended', 'plugin:prettier/recommended'],
     parserOptions: {
-        'sourceType': 'commonjs',
-        "ecmaVersion": 'latest'
+        sourceType: 'commonjs',
+        ecmaVersion: 'latest'
     },
     rules: {
         indent: 'off',
         semi: ['error', 'always'],
-        'arrow-body-style': ["error", "as-needed"],
-        "prettier/prettier": [
-          "error", {
-              "singleQuote": true,
-              "parser": "flow"
-          }
+        'arrow-body-style': ['error', 'as-needed'],
+        'prettier/prettier': [
+          'error', {
+            singleQuote: true,
+            parser: "flow",
+            tabWidth: 4
+          },
           ],
     },
     overrides: [
         {
-            'files': ['*/*.js'],
+            files: ['*/*.js'],
         },
     ],
-    'ignorePatterns': ['.eslintrc.js'],
+    ignorePatterns: ['.eslintrc.js'],
 };
