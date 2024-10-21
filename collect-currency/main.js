bcdf26416a164631249fdddafac87ec257ec6d4d
Redume <danilivasenko09@gmail.com>
2024-10-21 21:32:23 +0300

    Добавил логгер

--- collect-currency/main.js
+++ collect-currency/main.js
@@ -5,6 +5,8 @@ const cron = require('cron-validator');
 const save_fiat = require('./save_fiat');
 const save_crypto = require('./save_crypto');
 
+const logger = require('../shared/logger/src/main.js');
+
 async function validateSchedule(schedule) {
     if (!schedule) throw new Error('The crontab schedule is not set');
     if (!cron.isValidCron(schedule, { alias: true }))
