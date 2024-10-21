3cfc494ba676f62740e8ad4339a9e37e5d140a2c
Redume <danilivasenko09@gmail.com>
2024-10-21 21:31:55 +0300

    Рефакторинг кода

--- shared/database/src/create_table.js
+++ shared/database/src/create_table.js
@@ -1,17 +1,25 @@
 const fs = require('fs');
 const pool = require('./postgresql.js');
 
+const logger = require('../../logger/src/main.js');
+
 async function create_table() {
     const schema = fs.readFileSync(
         '../shared/database/data/schema.sql',
         'utf8',
     );
 
-    for (let i = 0; i < schema.split(';').length; i++) {
+    const queries = schema
+        .split(';')
+        .map((query) => query.trim())
+        .filter((query) => query);
+
+    for (const query of queries) {
         try {
-            await pool.query(schema.split(';')[i]);
+            await pool.query(query);
+            logger.info(`Executed query: ${query}`);
         } catch (err) {
-            continue;
+            logger.error(err);
         }
     }
 }
