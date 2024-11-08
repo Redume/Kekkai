cd01af2eac5239bce1926c2744f4f21c5ff2bb52
Redume <danilivasenko09@gmail.com>
2024-11-08 15:29:03 +0300

    feat(server): Errors from postgresql are also now in a unified response style

--- server/routes/getRate.js
+++ server/routes/getRate.js
@@ -12,34 +12,44 @@ module.exports = async function getRateRoute(fastify) {
         }
         let rate_res;
 
-        if (query['date'])
-            rate_res = await rate.getDay(
-                query['from_currency'],
-                query['conv_currency'],
-                query['date'],
-            );
-        else if (query['start_date'] && query['end_date'])
-            rate_res = await rate.getPeriod(
-                query['from_currency'],
-                query['conv_currency'],
-                query['start_date'],
-                query['end_date'],
-            );
-        else
-            return res.status(400).send({
-                status: 400,
-                message:
-                    'The date or period field is incorrect. ' +
-                    "There must be fields 'date' or 'start_date' and 'end_date'. " +
-                    'Read more in the documentation',
-            });
+        try {
 
-        if (typeof rate_res !== "object") return res.status(400).send({
-            status: 400,
-            message: rate_res,
-        });
-        else return res.status(200).send(
-            rate_res
-        )
+            if (query['date'])
+                rate_res = await rate.getDay(
+                    query['from_currency'],
+                    query['conv_currency'],
+                    query['date'],
+                );
+            else if (query['start_date'] && query['end_date'])
+                rate_res = await rate.getPeriod(
+                    query['from_currency'],
+                    query['conv_currency'],
+                    query['start_date'],
+                    query['end_date'],
+                );
+            else
+                return res.status(400).send({
+                    status: 400,
+                    message:
+                        'The date or period field is incorrect. ' +
+                        "There must be fields 'date' or 'start_date' and 'end_date'. " +
+                        'Read more in the documentation',
+                });
+
+
+            if (typeof rate_res !== "object") return res.status(400).send({
+                status: 400,
+                message: rate_res,
+            });
+            else return res.status(200).send(
+                rate_res
+            )
+        } catch (err) {
+            fastify.log.error(err.message);
+            res.status(500).send({
+                status: 500,
+                message: err.message
+            })
+        }
     });
 };
