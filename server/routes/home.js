0ae8fe3dea873f9772af338ee84e967fcdedfea6
Redume <danilivasenko09@gmail.com>
2024-11-08 15:23:55 +0300

    feat(server): Update link in home route for docs

--- server/routes/home.js
+++ server/routes/home.js
@@ -2,7 +2,7 @@ module.exports = async function getRateRoute(fastify) {
     fastify.get('/', async function (req, res) {
         return res.status(200).send({
             message: 'Hello World!',
-            documentation: 'https://github.com/Redume/Kekkai/wiki/',
+            documentation: 'https://kekkai-docs.redume.su/',
         });
     });
 };
