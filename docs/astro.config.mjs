a707bb2da7e4fd23940d11d975ca5cf7d9130b05
Redume <danilivasenko09@gmail.com>
2025-02-24 16:32:32 +0300

    docs: set base url /docs/

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -7,16 +7,17 @@ export default defineConfig({
 	integrations: [
 		starlight({
 			title: 'Kekkai',
-			social: {
-				github: 'https://github.com/redume/kekkai',
-			},
+			social: { github: 'https://github.com/redume/kekkai' },
+			editLink: { baseUrl: 'https://github.com/redume/kekkai/edit/main/docs/' },
 			sidebar: [
 				{
 					label: 'Getting started',
 					items: [
 						{
-							label: 'Docker', slug: 'getting-started/docker', badge: 'recommended'
-						}
+							label: 'Docker', 
+							slug: 'docs/getting-started/docker', 
+							badge: 'recommended',
+						}, 
 					],
 				},
 			],
