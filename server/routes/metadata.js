57867e2d73564e372e01323b62a1f5f1cee31e41
Redume <danilivasenko09@gmail.com>
2025-03-26 13:11:21 +0300

    chore: auto fix prettier, more compact code

--- server/routes/metadata.js
+++ server/routes/metadata.js
@@ -3,16 +3,24 @@ const config = require('../../shared/config/src/main.js')();
 
 module.exports = async function metadata(fastify) {
     fastify.get('/api/metadata/', async function (req, res) {
-        const first_date = await pool.query('SELECT * FROM currency ORDER BY date LIMIT 1');
-        const last_date = await pool.query('SELECT * FROM currency ORDER BY date DESC LIMIT 1');
-        
+        const first_date = await pool.query(
+            'SELECT * FROM currency ORDER BY date LIMIT 1',
+        );
+        const last_date = await pool.query(
+            'SELECT * FROM currency ORDER BY date DESC LIMIT 1',
+        );
+
         return res.status(200).send({
-            'first_date': first_date.rows[0]?.date ? first_date.rows[0]?.date : 'None',
-            'last_date': last_date.rows[0]?.date ? last_date.rows[0]?.date : 'None',
-            'currencies': {
-                'crypto': config['currency']['crypto'],
-                'fiat': config['currency']['fiat'],
-            }
-        })
+            first_date: first_date.rows[0]?.date
+                ? first_date.rows[0]?.date
+                : 'None',
+            last_date: last_date.rows[0]?.date
+                ? last_date.rows[0]?.date
+                : 'None',
+            currencies: {
+                crypto: config['currency']['crypto'],
+                fiat: config['currency']['fiat'],
+            },
+        });
     });
-}
+};
