afe05535b4a4fb6c88d81468bb4002499dff1da1
Redume <danilivasenko09@gmail.com>
2025-04-10 01:35:26 +0300

    chore(chart): Added docstring to the dto schema

--- chart/schemas/currency.py
+++ chart/schemas/currency.py
@@ -1,24 +1,41 @@
+"""
+This module provides a model for handling currency conversion operations.
+"""
 from datetime import date
 from typing import Optional
 
 from pydantic import BaseModel, Field, root_validator
 
 class Currency(BaseModel):
+    """
+    A model that describes the parameters for currency conversion.
+
+    Attributes:
+        from_currency (str): The currency to convert from.
+        conv_currency (str): The currency to convert to.
+        start_date (Optional[date]): The beginning date of the period.
+        end_date (Optional[date]): The end date of the period.
+        period (Optional[str]): A date period parameter 
+            that can be 'week', 'month', 'quarter', or 'year'.
+    """
     from_currency: str = Field(
         ...,
         description='Currencies from which conversion takes place'
-        )
+    )
     conv_currency: str = Field(
         ...,
         description='Currency to which conversion takes place'
-        )
+    )
     start_date: Optional[date] = Field(
         default=None,
         description='Beginnings of the period'
     )
-    end_date: Optional[date] = Field(default=None, description='End of period')
+    end_date: Optional[date] = Field(
+        default=None,
+        description='End of period'
+    )
     period: Optional[str] = Field(
         default=None,
-        description='Date Period. ' \
+        description='Date Period.' \
         'Arguments week/month/quarter/year are accepted'
     )
