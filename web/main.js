3af061a72f7314c800c1b54a46989a34b52e276e
Redume <danilivasenko09@gmail.com>
2025-05-28 20:14:12 +0300

    chore: Removed SSL certificates

--- web/main.js
+++ web/main.js
@@ -1,25 +1,10 @@
 const logger = require('../shared/logger');
 const config = require('../shared/config/src/main.js')();
 
-const fs = require('fs');
 const path = require('node:path');
 
 const fastify = require('fastify')({
     logger: config['server']['log']['level'] !== 'none' ? logger : false,
-    ...(config['server']['ssl']['enabled']
-        ? {
-              https: {
-                  key: fs.readFileSync(
-                      config['server']['ssl']['private_key'],
-                      'utf8',
-                  ),
-                  cert: fs.readFileSync(
-                      config['server']['ssl']['cert'],
-                      'utf8',
-                  ),
-              },
-          }
-        : false),
 });
 
 fastify.register(require('@fastify/static'), {
