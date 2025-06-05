d72269be4519cad6f07af7866cb9ccd46d355438
Redume <danilivasenko09@gmail.com>
2025-06-05 11:54:44 +0300

    chore: The database schema file has been moved to shared. The paths have been updated, the file is copied to dockerfile

--- shared/database/src/create_table.js
+++ shared/database/src/create_table.js
@@ -1,18 +1,17 @@
 const fs = require('fs');
+const path = require('path');
 const pool = require('./postgresql.js');
 
 const logger = require('../../logger/src/main.js');
 
 async function create_table() {
-    const schema = fs.readFileSync(
-        '../shared/database/schemas/data.sql',
-        'utf8',
-    );
+    const schemaPath = path.join(__dirname, '../schemas/data.sql');
+    const schemaContent = fs.readFileSync(schemaPath, 'utf8');
 
-    const queries = schema
+    const queries = schemaContent
         .split(';')
         .map((query) => query.trim())
-        .filter((query) => query);
+        .filter((query) => query.length > 0);
 
     for (const query of queries) {
         try {
