55e7e29e0249b1ee1a13b0a41660b646016334c3
Redume <danilivasenko09@gmail.com>
2025-06-05 12:27:59 +0300

    chore(web): The backend for the web (/) has been removed and the page goes through nginx

--- web/routes/home.js
+++
@@ -1,7 +0,0 @@
-const fs = require("node:fs");
-
-module.exports = async function homeRoute(fastify) {
-    fastify.get('/', (req, res) => {
-        res.send(fs.createReadStream('./src/html/index.html'));
-    });
-}
